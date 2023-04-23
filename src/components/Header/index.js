357aeda252380e9e0c3381ec056f0670f1dad33c
Vitor Santos <pireshugo737@gmail.com>
2023-04-23 08:56:56 -0300

    app: initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="title">GitFind</h1>
+        </header>
+    );
+};
+
+export default Header;
